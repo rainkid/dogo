745fc49379ce1bbec63f2ff1365268e6fa3df03c
root <rainkide@gmail.com>
2014-12-10 21:07:39 +0800

    update

---
+++ loger.go
@@ -0,0 +1,46 @@
+package dogo
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+type MyLoger struct {
+	infoType string
+	Handler  *log.Logger
+	Infos    []interface{}
+}
+
+func NewLoger() *MyLoger {
+	return &MyLoger{
+		Handler: log.New(os.Stdout, "", log.Ldate|log.Ltime),
+	}
+}
+
+func (l *MyLoger) D(infos ...interface{}) {
+	l.infoType = "DEBUG"
+	l.Infos = infos
+	l.P()
+}
+
+func (l *MyLoger) I(infos ...interface{}) {
+	l.infoType = "INFO"
+	l.Infos = infos
+	l.P()
+}
+
+func (l *MyLoger) E(infos ...interface{}) {
+	l.infoType = "ERROR"
+	l.Infos = infos
+	l.P()
+}
+
+func (l *MyLoger) P() {
+	var s string
+	for _, v := range l.Infos {
+		s += fmt.Sprintf("%v ", v)
+	}
+	l.Handler.SetPrefix(fmt.Sprintf("%s - ", l.infoType))
+	l.Handler.Println(fmt.Sprintf(` "%s"`, s))
+}
